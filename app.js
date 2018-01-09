51dbb2c74d7c093bce158e8728bad059e9c276c4
bikashpattnayak <bikash.pattnayak@gmail.com>
2018-01-10 02:21:43 +0530

    connected client to server

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const express = require('express');
 const path = require('path');
 const publicPath = path.join(__dirname,"public");
 const app = express();
+const http = require('http');
+const socketIo = require('socket.io');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.static(publicPath));
+var server = http.createServer(app);
+var io = socketIo(server);
 
 
+io.on('connection', (socket) => {
+	console.log('New Connection received');
+
+	socket.on('disconnect', () => {
+		console.log('Client disconnected');
+	})
+
+})
 
 
-
-app.listen(port,() => {
+server.listen(port,() => {
 	console.log(`Server is running on ${port}`);
 
 });
